9c1ffd52998da23fa5c29d227ab1b0a8a1f76bc5
la casa de burger <ton-email@exemple.com>
2026-03-07 20:41:48 +0100

    update

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,61 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react-swc'
+import viteCompression from 'vite-plugin-compression'
 
 export default defineConfig({
+  plugins: [
+    react(),
+    viteCompression({
+      algorithm: 'brotliCompress',
+      ext: '.br',
+      threshold: 1024,
+    }),
+    viteCompression({
+      algorithm: 'gzip',
+      ext: '.gz',
+      threshold: 1024,
+    })
+  ],
   base: '/',
-  plugins: [react()],
   build: {
+    emptyOutDir: true,
+    assetsDir: 'assets',
+    target: 'esnext',
     cssCodeSplit: false,
+    modulePreload: {
+      polyfill: false,
+    },
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true,
+        pure_funcs: ['console.info', 'console.debug', 'console.warn', 'console.log'],
+        passes: 3,
+      },
+      output: {
+        comments: false,
+      },
+    },
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name]-[hash].js`,
+        chunkFileNames: `assets/[name]-[hash].js`,
+        assetFileNames: `assets/[name]-[hash].[ext]`,
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('react') || id.includes('react-dom') || id.includes('scheduler')) {
+              return 'vendor-core'
+            }
+            return 'vendor-utils'
+          }
+        },
+      },
+    },
+    chunkSizeWarningLimit: 600,
+    reportCompressedSize: false,
+  },
+  optimizeDeps: {
+    include: ['react', 'react-dom'],
   }
 })
