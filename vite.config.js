342d5077e612c7ad214b59a191193fa55d2f7951
la casa de burger <ton-email@exemple.com>
2026-03-07 14:56:41 +0100

    update

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Reto-33/',
+  base: '/',
   plugins: [react()],
   build: {
     cssCodeSplit: false,
